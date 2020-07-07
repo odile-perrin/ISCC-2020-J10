3676b244b0d74cd944fc7108b4b5ef1279bc9901
odpr <67262187+odpr@users.noreply.github.com>
2020-07-07 12:54:31 +0200

    Push 2

--- EX_01/insertion-produit.sql
+++ EX_01/insertion-produit.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Hôte : 127.0.0.1
--- Généré le : mar. 07 juil. 2020 à 11:30
+-- Généré le : mar. 07 juil. 2020 à 12:29
 -- Version du serveur :  10.4.13-MariaDB
 -- Version de PHP : 7.4.7
 
@@ -31,7 +31,7 @@ CREATE TABLE `produit` (
   `id` int(11) NOT NULL,
   `nom` tinytext NOT NULL,
   `desc` longtext NOT NULL,
-  `prix` decimal(10,0) NOT NULL,
+  `prix` float NOT NULL,
   `quantite` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
@@ -40,13 +40,13 @@ CREATE TABLE `produit` (
 --
 
 INSERT INTO `produit` (`id`, `nom`, `desc`, `prix`, `quantite`) VALUES
-(1, 'T-shirt rouge', 'T-shirt coton de couleur rouge', '16', 5),
-(2, 'T-shirt vert', 'T-shirt coton de couleur verte', '16', 6),
-(3, 'T-shirt argent', 'T-shirt coton de couleur argent', '16', 8),
-(4, 'Short bleu', 'Short jean de couleur bleue', '17', 5),
-(5, 'Short vert', 'Short jean de couleur verte', '20', 5),
-(6, 'Gilet noir', 'Gilet de couleur noire', '20', 10),
-(7, 'Veste argent', 'Veste de couleur argentée', '35', 3);
+(1, 'T-shirt rouge', 'T-shirt coton de couleur rouge', 15.5, 5),
+(2, 'T-shirt vert', 'T-shirt coton de couleur verte', 15.5, 6),
+(3, 'T-shirt argent', 'T-shirt de couleur argent', 15.5, 8),
+(4, 'Short bleu', 'Short jean de couleur bleue', 16.5, 5),
+(5, 'Short vert', 'Short jean de couleur verte', 19.99, 5),
+(6, 'Gilet noir', 'Gilet de couleur noire', 19.99, 10),
+(7, 'Veste argent', 'Veste de couleur argentée', 35, 3);
 
 --
 -- Index pour les tables déchargées
---
+++ EX_01/requete-select.sql
@@ -0,0 +1,7 @@
+SELECT * FROM produit
+
+SELECT * FROM produit WHERE nom LIKE 'T-shirt%'
+
+SELECT * FROM produit WHERE prix LIKE '19.99%'
+
+SELECT * FROM produit WHERE quantite LIKE '5%'
